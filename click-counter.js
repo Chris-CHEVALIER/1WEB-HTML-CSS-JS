a8ee03ad0f473404fb4e0e0212fe6eb01645c0b1
Chris <chevalier@chris-freelance.com>
2025-10-22 15:20:30 +0200

    Add js

---
+++ click-counter.js
@@ -0,0 +1,53 @@
+// console.log("Script chargé !");
+
+let countLabel = document.getElementById('count-label')
+let clickBtn = document.querySelector('#click-btn')
+
+let count = 0
+clickBtn.addEventListener('click', function () {
+  count++
+  countLabel.textContent = count
+})
+
+// Sélection des éléments
+const button = document.getElementById('change-theme-button')
+
+// Ajout de l'écouteur d'événement
+button.addEventListener('click', changeColor)
+
+// Fonction pour changer la couleur
+function changeColor () {
+  if (document.body.style.backgroundColor == 'black') {
+    document.body.style.backgroundColor = 'white' // Change la couleur de fond de la page
+    document.body.style.color = 'black'
+  } else {
+    document.body.style.backgroundColor = 'black' // Change la couleur de fond de la page
+    document.body.style.color = 'white'
+  }
+}
+
+const showButton = document.getElementById('show-button');
+const hideButton = document.getElementById('hide-button');
+const hiddenText = document.getElementById('hidden-text');
+
+showButton.addEventListener('click', function() {
+    hiddenText.style.display = 'block'; // Affiche l'élément
+})
+
+hideButton.addEventListener('click', function() {
+    hiddenText.style.display = 'none'; // Cache l'élément
+})
+
+const input = document.getElementById('item');
+const btn = document.getElementById('add-button');
+const list = document.getElementById('list');
+
+btn.addEventListener('click', function() {
+    const text = input.value;
+    if (text) {
+        const newItem = document.createElement('li');
+        newItem.textContent = text;
+        list.appendChild(newItem);
+        input.value = ''; // Réinitialise le champ de texte
+    }
+})
---
+++ script.js
@@ -0,0 +1,35 @@
+console.log(document)
+let siamoisTitle = document.getElementById('siamois')
+console.log(siamoisTitle)
+
+/* let h2Titles = document.querySelectorAll("h2")
+console.log(h2Titles); */
+
+siamoisTitle.textContent = 'Les super siamois ! 😺'
+siamoisTitle.style.backgroundColor = 'dodgerblue'
+siamoisTitle.style.padding = '16px'
+
+let newSection = document.createElement('section') // <section></section>
+let newH2 = document.createElement('h2') // <h2></h2>
+newH2.textContent = 'Les lions 🦁' // <h2>Les lions</h2>
+let newImg = document.createElement('img') // <img />
+newImg.src =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/011_The_lion_king_Tryggve_in_the_Serengeti_National_Park_Photo_by_Giles_Laurent.jpg/960px-011_The_lion_king_Tryggve_in_the_Serengeti_National_Park_Photo_by_Giles_Laurent.jpg' // <img src="..." />
+newImg.alt = 'Un lion 🦁' // <img src="..." alt="Un lion 🦁" />
+
+console.log(newH2, newImg)
+
+newSection.appendChild(newH2)
+newSection.appendChild(newImg)
+
+let main = document.querySelector("main")
+main.appendChild(newSection)
+
+/* newSection.removeChild(newH2)
+newSection.removeChild(newImg) */
+/*
+ <section>
+    <h2>Les lions 🦁</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/011_The_lion_king_Tryggve_in_the_Serengeti_National_Park_Photo_by_Giles_Laurent.jpg/960px-011_The_lion_king_Tryggve_in_the_Serengeti_National_Park_Photo_by_Giles_Laurent.jpg" alt="Un lion 🦁">    
+</section>
+*/
